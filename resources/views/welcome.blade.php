7a36c076614fb615683df884e677a2d965829bf1
haiou <2111255898@qq.com>
2019-12-30 16:10:46 +0800

    111

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    API-1905
+                    API_1905
                 </div>
 
                 <div class="links">
